3c4ae95d07a470ed0e1f1137fbf59227cb446847
Lantis Gurr <jlantisgurr@gmail.com>
2025-04-25 20:57:50 -0700

    8:57 PM

--- main.hpp
+++ main.hpp
@@ -1,8 +1,39 @@
+#include <iostream>
+using namespace std;
 
-//********************
-// Declare Function Prototypes
-//********************
+void getTwoValues(int &begin, int &end) {
+    cout << "Insert two integer values: " << endl;
+    cin >> begin >> end;
+    if (begin >= end) {
+        cout << "Invalid input. \n";
+    }
+while (begin >= end);
+}
+bool Prime(int n) {
+    if (n <= 1) {
+        return false; 
+        }
+    
+        for (int i = 2; i * i <= n; ++i) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
 
-// ******************************
-// Implement all your functions here
-// ******************************
+int getNextPrime(int begin) {
+    int num = begin + 1;
+    while (!Prime(num)) {
+        ++num;
+    }
+    return num;
+}
+
+int getPrevPrime(int end) {
+    int num = end - 1;
+    while (num > 1 && !Prime(num)) {
+        --num;
+    }
+    return num > 1 ? num : -1; 
+}
